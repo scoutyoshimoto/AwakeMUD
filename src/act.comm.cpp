8febf4668157921f8fb69230b137969dab7c0634
scoutyoshimoto <classes-snaps-0w@icloud.com>
2026-07-27 23:43:52 +1000

    fix otaku radios and matrix comma broadcasts

--- src/act.comm.cpp
+++ src/act.comm.cpp
@@ -940,9 +940,14 @@ ACMD(do_broadcast)
   }
 
   radio = find_radio(ch, &cyberware, &vehicle, &matrix);
-  if (PLR_FLAGGED(ch, PLR_MATRIX) && !matrix) {
-    send_to_char("You can't access the radio frequencies without a radio link.\r\n", ch);
-    return;
+  if (PLR_FLAGGED(ch, PLR_MATRIX) && !matrix && !IS_OTAKU(ch)) {
+    if (IS_OTAKU(ch) && !cyberware) {
+      send_to_char("You can't access the radio frequencies without a radio complex form, and a cyber radio.\r\n", ch);
+      return;
+    } else if (!matrix) {
+      send_to_char("You can't access the radio frequencies without a radio link.\r\n", ch);
+      return;
+    }
   }
 
   if (IS_NPC(ch) || IS_SENATOR(ch)) {
